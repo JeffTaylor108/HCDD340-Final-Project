d2625445a2c50c1b328d61a9e79a53dbaf690df5
JeffTaylor108 <jeffta1080@gmail.com>
2023-12-10 20:07:45 -0500

    made rating bars functional and save data persistently made menu functional and added some icons, still need Account icon and Milestones icon

--- app/src/main/java/edu/psu/ist/hcdd340/finalproject/PlanningListActivity.java
+++ app/src/main/java/edu/psu/ist/hcdd340/finalproject/PlanningListActivity.java
@@ -2,23 +2,35 @@ package edu.psu.ist.hcdd340.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
+import android.media.Rating;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.RatingBar;
 
-public class MainActivity extends AppCompatActivity {
+public class PlanningListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_activity);
+        setContentView(R.layout.plan_watch_activity);
 
+        RatingBar theBearRB = findViewById(R.id.theBearRatingBar);
+        RatingBar youRB = findViewById(R.id.youRatingBar);
+        RatingBar theBoysRB = findViewById(R.id.theBoysRatingBar);
 
+        // Sets AVG Rating
+        theBearRB.setRating(4.5f);
+        youRB.setRating(4f);
+        theBoysRB.setRating(4.5f);
+
+        // Disables rating bars
+        theBearRB.setIsIndicator(true);
+        youRB.setIsIndicator(true);
+        theBoysRB.setIsIndicator(true);
     }
+
     // Menu Handling
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -33,12 +45,11 @@ public class MainActivity extends AppCompatActivity {
             startActivity(watchListIntent);
             return true;
         }
-        else if (menuId == R.id.menu_planning) {
+        if (menuId == R.id.menu_planning) {
             Intent planningListIntent = new Intent(this, PlanningListActivity.class);
             startActivity(planningListIntent);
             return true;
         }
         return false;
     }
-
 }
---
+++ app/src/main/java/edu/psu/ist/hcdd340/finalproject/WatchListActivity.java
@@ -0,0 +1,89 @@
+package edu.psu.ist.hcdd340.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.RatingBar;
+
+public class WatchListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.watch_list_activity);
+
+        //Creates data saving object for ratings
+        SharedPreferences watchListRatings = getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = watchListRatings.edit();
+
+        RatingBar parasiteRatingBar = findViewById(R.id.parasiteRatingBar);
+        RatingBar bladeRunnerRatingBar = findViewById(R.id.bladeRunnerRatingBar);
+        RatingBar barbieRatingBar = findViewById(R.id.barbieRatingBar);
+
+        //Sets the data saved in watchListRatings to what is displayed
+        float parasiteSavedRating = watchListRatings.getFloat("parasite", 0.0f);
+        float bladeRunnerSavedRating = watchListRatings.getFloat("blade runner", 0.0f);
+        float barbieSavedRating = watchListRatings.getFloat("barbie", 0.0f);
+
+        parasiteRatingBar.setRating(parasiteSavedRating);
+        bladeRunnerRatingBar.setRating(bladeRunnerSavedRating);
+        barbieRatingBar.setRating(barbieSavedRating);
+        parasiteRatingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                ratingBar.setRating(rating);
+                //applies changes to SharedPreferences object
+                editor.putFloat("parasite", ratingBar.getRating());
+                editor.apply();
+
+            }
+        });
+        bladeRunnerRatingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                ratingBar.setRating(rating);
+                //applies changes to SharedPreferences object
+                editor.putFloat("blade runner", ratingBar.getRating());
+                editor.apply();
+
+            }
+        });
+        barbieRatingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                ratingBar.setRating(rating);
+                //applies changes to SharedPreferences object
+                editor.putFloat("barbie", ratingBar.getRating());
+                editor.apply();
+
+            }
+        });
+    }
+
+    // Menu Handling
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int menuId = item.getItemId();
+        if (menuId == R.id.menu_watched) {
+            Intent watchListIntent = new Intent(this, WatchListActivity.class);
+            startActivity(watchListIntent);
+            return true;
+        }
+        else if (menuId == R.id.menu_planning) {
+            Intent planningListIntent = new Intent(this, PlanningListActivity.class);
+            startActivity(planningListIntent);
+            return true;
+        }
+        return false;
+    }
+}
