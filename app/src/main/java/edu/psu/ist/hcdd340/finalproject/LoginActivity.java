fda6a0c7368e89cf0e1dbd3dc3713883f5864350
snkenny <samanthankenny@gmail.com>
2023-12-11 17:08:11 -0500

    Login functionality attempt 1

---
+++ app/src/main/java/edu/psu/ist/hcdd340/finalproject/LoginActivity.java
@@ -0,0 +1,35 @@
+package edu.psu.ist.hcdd340.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+
+        Button button = findViewById(R.id.loginButton);
+        button.setOnClickListener(this);
+    }
+
+    private final static String TAG = "LOG_IN";
+
+    @Override
+    public void onClick(View view) {
+        EditText emailEditText = findViewById(R.id.editTextEmail);
+        String email = emailEditText.getText().toString();;
+
+        EditText passwordEditText = findViewById(R.id.editTextPassword);
+        String password = passwordEditText.getText().toString();
+
+        Log.d(TAG, "Email entered: " + email + " and Password: " + password);
+
+    }
+}
